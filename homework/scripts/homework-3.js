b424b122a9b024a6ae7ffb750720c6395d5e5202
Olena <olena@example.com>
2026-05-09 19:52:31 +0300

    Add homework 3 object tasks

--- homework/scripts/homework-3.js
+++ homework/scripts/homework-3.js
@@ -1,3 +1,51 @@
 "use strict";
 
-// Homework 3
+const car = {
+  manufacturer: "Toyota",
+  model: "Corolla",
+  year: 2020,
+  averageSpeed: 90,
+};
+
+function showCarInfo(carInfo) {
+  alert(
+    `Інформація про автомобіль:
+Виробник: ${carInfo.manufacturer}
+Модель: ${carInfo.model}
+Рік випуску: ${carInfo.year}
+Середня швидкість: ${carInfo.averageSpeed} км/год`
+  );
+}
+
+function calculateTravelTime(distance, carInfo) {
+  const drivingTime = distance / carInfo.averageSpeed;
+  const breaksCount = Math.floor(drivingTime / 4);
+
+  return drivingTime + breaksCount;
+}
+
+showCarInfo(car);
+
+const distance = Number(prompt("Введіть відстань у кілометрах:"));
+const travelTime = calculateTravelTime(distance, car);
+
+alert(`Необхідний час у дорозі: ${travelTime} год.`);
+
+const printMachine = {
+  fontSize: "20px",
+  fontColor: "blue",
+  fontFamily: "Arial",
+  print(text) {
+    alert(
+      `Інформація про printMachine:
+Розмір шрифту: ${this.fontSize}
+Колір шрифту: ${this.fontColor}
+Сімейство шрифту: ${this.fontFamily}
+Текст: ${text}`
+    );
+  },
+};
+
+setTimeout(function () {
+  printMachine.print("Hello, JavaScript!");
+}, 5000);
